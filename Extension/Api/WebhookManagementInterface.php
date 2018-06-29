1d07ceef508224c8f9f7e5b52da4ec7e04cb2abb
Ron van der Molen <ron@wizkunde.nl>
2018-06-29 10:22:53 +0200

    Add webhook management api interface

---
+++ Extension/Api/WebhookManagementInterface.php
@@ -0,0 +1,23 @@
+<?php
+namespace Unific\Extension\Api;
+
+interface WebhookManagementInterface
+{
+    /**
+     * Sets the group of a webhook
+     *
+     * @api
+     * @param string $group
+     * @return bool true on success
+     */
+    public function setGroup($group = 'default');
+
+    /**
+     * Returns the mode
+     *
+     * @api
+     *
+     * @return string
+     */
+    public function getGroup();
+}
