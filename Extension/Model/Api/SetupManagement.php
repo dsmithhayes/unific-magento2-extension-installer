dffc68435cd785d968a67a7f70628a2e66f1d475
Ron van der Molen <ron@wizkunde.nl>
2018-08-29 22:03:47 +0200

    Change get to POST request

--- Extension/Model/Api/SetupManagement.php
+++ Extension/Model/Api/SetupManagement.php
@@ -12,14 +12,21 @@ class SetupManagement implements SetupManagementInterface
     protected $scopeConfig;
 
     /**
+     * @var \Magento\Framework\App\Request\Http
+     */
+    protected $request;
+
+    /**
      * ModeManagement constructor.
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
      */
     public function __construct(
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\App\Request\Http $request
     )
     {
         $this->scopeConfig = $scopeConfig;
+        $this->request = $request;
     }
 
     /**
@@ -31,6 +38,8 @@ class SetupManagement implements SetupManagementInterface
      */
     public function getData()
     {
+        $this->scopeConfig->setValue('unific/extension/integration', $this->request->getPost('integration_id'), \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+
         return array('hmac' => array(
             'hmac_header' => $this->scopeConfig->getValue('unific/hmac/hmacHeader', \Magento\Store\Model\ScopeInterface::SCOPE_STORE),
             'hmac_secret' => $this->scopeConfig->getValue('unific/hmac/hmacSecret', \Magento\Store\Model\ScopeInterface::SCOPE_STORE),
