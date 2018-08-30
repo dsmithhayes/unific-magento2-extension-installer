cb02f0916e9dd63e3fe18984a97d88e020ecf330
Ron van der Molen <ron@wizkunde.nl>
2018-08-30 13:05:20 +0200

    Add classes for the interfaces

---
+++ Extension/Model/Api/Data/SetupResponse.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Unific\Extension\Model\Api\Data;
+
+class Hmac implements \Unific\Extension\Api\Data\SetupResponseInterface
+{
+    /**
+     * @var
+     */
+    protected $hmac;
+
+    /**
+     * @var
+     */
+    protected $totals;
+
+    /**
+     * @return \Unific\Extension\Api\Data\HmacInterface
+     */
+    public function getHmac()
+    {
+        return $this->hmac;
+    }
+
+    /**
+     * @param \Unific\Extension\Api\Data\HmacInterface $hmac
+     */
+    public function setHmac(\Unific\Extension\Api\Data\HmacInterface $hmac)
+    {
+        $this->hmac = $hmac;
+    }
+
+    /**
+     * @return \Unific\Extension\Api\Data\TotalsInterface
+     */
+    public function getTotals()
+    {
+        return $this->totals;
+    }
+
+    /**
+     * @param \Unific\Extension\Api\Data\TotalsInterface $totals
+     */
+    public function setTotals(\Unific\Extension\Api\Data\TotalsInterface $totals)
+    {
+        $this->totals = $totals;
+    }
+
+
+}
