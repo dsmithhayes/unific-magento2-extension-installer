f6f2f81a3db56217d83b42d7820978a8c49146ad
Ron van der Molen <ron@wizkunde.nl>
2018-08-31 08:59:18 +0200

    Make the event match the subject of unific

--- Extension/Helper/Webhook.php
+++ Extension/Helper/Webhook.php
@@ -13,23 +13,23 @@ class Webhook extends \Magento\Framework\App\Helper\AbstractHelper
      * @var array
      */
     private $actionmapping = array(
-        'customer_login' => 'Magento\Customer\Model\Session::setCustomerAsLoggedIn',
-        'customer_logout' => 'Magento\Customer\Model\Session::logout',
-        'admin_login' => 'Magento\Backend\Model\Auth\Session::processLogin',
-        'admin_logout' => 'Magento\Backend\Model\Auth\Session::processLogout',
-        'customer_create' => 'Magento\Customer\Api\CustomerManagementInterface::save',
-        'customer_update' => 'Magento\Customer\Api\CustomerManagementInterface::save',
-        'admin_user_create' => 'Magento\User\Model\User::save',
-        'quote_create' => 'Magento\Quote\Api\CartManagementInterface::save',
-        'quote_update' => 'Magento\Quote\Api\CartManagementInterface::save',
-        'order_create' => 'Magento\Sales\Api\OrderManagementInterface::place',
-        'invoice_create' => 'Magento\Sales\Model\Order\Invoice::capture',
-        'creditmemo_create' => 'Magento\Sales\Model\Order\Creditmemo::save',
-        'shipment_create' => 'Magento\Shipment\Model\Shipment::save',
-        'category_create' => 'Magento\Catalog\Api\CategoryManagementInterface::save',
-        'category_update' => 'Magento\Catalog\Api\CategoryManagementInterface::save',
-        'product_create' => 'Magento\Catalog\Api\ProductManagementInterface::save',
-        'product_update' => 'Magento\Catalog\Api\ProductManagementInterface::save'
+        'customer/login' => 'Magento\Customer\Model\Session::setCustomerAsLoggedIn',
+        'customer/logout' => 'Magento\Customer\Model\Session::logout',
+        'admin/login' => 'Magento\Backend\Model\Auth\Session::processLogin',
+        'admin/logout' => 'Magento\Backend\Model\Auth\Session::processLogout',
+        'customer/create' => 'Magento\Customer\Api\CustomerManagementInterface::save',
+        'customer/update' => 'Magento\Customer\Api\CustomerManagementInterface::save',
+        'admin/user/create' => 'Magento\User\Model\User::save',
+        'quote/create' => 'Magento\Quote\Api\CartManagementInterface::save',
+        'quote/update' => 'Magento\Quote\Api\CartManagementInterface::save',
+        'order/create' => 'Magento\Sales\Api\OrderManagementInterface::place',
+        'invoice/create' => 'Magento\Sales\Model\Order\Invoice::capture',
+        'creditmemo/create' => 'Magento\Sales\Model\Order\Creditmemo::save',
+        'shipment/create' => 'Magento\Shipment\Model\Shipment::save',
+        'category/create' => 'Magento\Catalog\Api\CategoryManagementInterface::save',
+        'category/update' => 'Magento\Catalog\Api\CategoryManagementInterface::save',
+        'product/create' => 'Magento\Catalog\Api\ProductManagementInterface::save',
+        'product/update' => 'Magento\Catalog\Api\ProductManagementInterface::save'
     );
 
     /**
