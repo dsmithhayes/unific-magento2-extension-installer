032026ff429cf2f0a36586c8808ef15e11719da5
Ron van der Molen <ron@wizkunde.nl>
2018-08-31 21:13:23 +0200

    Handling order fetching

--- Extension/Plugin/OrderPlugin.php
+++ Extension/Plugin/OrderPlugin.php
@@ -7,6 +7,32 @@ class OrderPlugin extends AbstractPlugin
     protected $entity = 'order';
     protected $subject = 'order/create';
 
+    protected $orderRepository;
+
+    /**
+     * OrderPlugin constructor.
+     * @param \Unific\Extension\Logger\Logger $logger
+     * @param \Unific\Extension\Helper\Mapping $mapping
+     * @param \Unific\Extension\Connection\Rest\Connection $restConnection
+     * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
+     */
+    public function __construct(
+        \Unific\Extension\Logger\Logger $logger,
+        \Unific\Extension\Helper\Mapping $mapping,
+        \Unific\Extension\Connection\Rest\Connection $restConnection,
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
+    )
+    {
+        $this->objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+        $this->logger = $logger;
+        $this->mappingHelper = $mapping;
+        $this->restConnection = $restConnection;
+        $this->orderRepository = $orderRepository;
+
+        parent::__construct($logger, $mapping, $restConnection);
+    }
+
     /**
      * @param $subject
      * @param $order
@@ -18,6 +44,8 @@ class OrderPlugin extends AbstractPlugin
                      ->addFieldToFilter('request_event', array('eq' => 'Magento\Sales\Api\OrderManagementInterface::place'))
                      ->addFieldToFilter('request_event_execution', array('eq' => 'before'))
                  as $id => $request) {
+
+            $order = $this->orderRepository->get($order->getId());
 
             $this->handleCondition($id, $request, $order);
         }
@@ -36,6 +64,8 @@ class OrderPlugin extends AbstractPlugin
                      ->addFieldToFilter('request_event', array('eq' => 'Magento\Sales\Api\OrderManagementInterface::place'))
                      ->addFieldToFilter('request_event_execution', array('eq' => 'after'))
                  as $id => $request) {
+
+            $order = $this->orderRepository->get($order->getId());
 
             $this->handleCondition($id, $request, $order);
         }
