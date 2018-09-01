47857c5ef02d571b7216ff825bbedaf1c0f1e3ec
Ron van der Molen <ron@wizkunde.nl>
2018-09-01 20:11:06 +0200

    Remove unneeded parameters

--- Extension/Plugin/CustomerPlugin.php
+++ Extension/Plugin/CustomerPlugin.php
@@ -36,11 +36,9 @@ class CustomerPlugin extends AbstractPlugin
     /**
      * @param $subject
      * @param $customer
-     * @param $password
-     * @param $redirectUrl
      * @return array
      */
-    public function beforeCreateAccount($subject, $customer, $password, $redirectUrl)
+    public function beforeCreateAccount($subject, $customer)
     {
         foreach ($this->getRequestCollection()
                      ->addFieldToFilter('request_event', array('eq' => 'Magento\Customer\Api\AccountManagementInterface::createAccount'))
@@ -50,17 +48,15 @@ class CustomerPlugin extends AbstractPlugin
             $this->handleCondition($id, $request, $customer);
         }
 
-        return [$customer, $password, $redirectUrl];
+        return [$customer];
     }
 
     /**
      * @param $subject
      * @param $customer
-     * @param $password
-     * @param $redirectUrl
      * @return mixed
      */
-    public function afterCreateAccount($subject, $customer, $password, $redirectUrl)
+    public function afterCreateAccount($subject, $customer)
     {
         foreach ($this->getRequestCollection()
                      ->addFieldToFilter('request_event', array('eq' => 'Magento\Customer\Api\AccountManagementInterface::createAccount'))
