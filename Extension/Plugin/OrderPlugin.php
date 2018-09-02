dced16a8df9f62550f0c94ee61a03a72427b2712
Ron van der Molen <ron@wizkunde.nl>
2018-09-02 22:08:26 +0200

    Use our own virtual type

--- Extension/Plugin/OrderPlugin.php
+++ Extension/Plugin/OrderPlugin.php
@@ -17,7 +17,7 @@ class OrderPlugin extends AbstractPlugin
      * @param \Unific\Extension\Helper\Mapping $mapping
      * @param \Unific\Extension\Connection\Rest\Connection $restConnection
      * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
-     * @param \Magento\Sales\Model\ResourceModel\Metadata $metadata
+     * @param \Unific\Extension\Model\ResourceModel\OrderMetadata $metadata
      * @param \Magento\Sales\Api\Data\OrderExtensionFactory|null $orderExtensionFactory
      */
     public function __construct(
@@ -25,7 +25,7 @@ class OrderPlugin extends AbstractPlugin
         \Unific\Extension\Helper\Mapping $mapping,
         \Unific\Extension\Connection\Rest\Connection $restConnection,
         \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
-        \Magento\Sales\Model\ResourceModel\Metadata $metadata,
+        \Unific\Extension\Model\ResourceModel\OrderMetadata $metadata,
         \Magento\Sales\Api\Data\OrderExtensionFactory $orderExtensionFactory = null
     )
     {
