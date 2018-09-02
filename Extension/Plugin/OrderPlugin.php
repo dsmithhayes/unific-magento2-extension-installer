12cdfae5ccbdd838e7d64884ace8ca00a80e01e8
Ron van der Molen <ron@wizkunde.nl>
2018-09-02 21:54:28 +0200

    Add extension data

--- Extension/Plugin/OrderPlugin.php
+++ Extension/Plugin/OrderPlugin.php
@@ -7,65 +7,31 @@ class OrderPlugin extends AbstractPlugin
     protected $entity = 'order';
     protected $subject = 'order/create';
 
-    /**
-     * @var Metadata
-     */
-    protected $metadata;
-
-    /**
-     * @var SearchResultFactory
-     */
-    protected $searchResultFactory = null;
-
-    /**
-     * @var OrderExtensionFactory
-     */
-    private $orderExtensionFactory;
-
-    /**
-     * @var ShippingAssignmentBuilder
-     */
-    private $shippingAssignmentBuilder;
-
-    /**
-     * @var CollectionProcessorInterface
-     */
-    private $collectionProcessor;
-
-    /**
-     * @var OrderInterface[]
-     */
-    protected $registry = [];
-
+    protected $orderRepository;
+    protected $orderExtensionFactory;
 
     /**
      * OrderPlugin constructor.
      * @param \Unific\Extension\Logger\Logger $logger
      * @param \Unific\Extension\Helper\Mapping $mapping
      * @param \Unific\Extension\Connection\Rest\Connection $restConnection
-     * @param \Magento\Sales\Api\Data\OrderSearchResultInterfaceFactory $searchResultFactory
-     * @param \Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface|null $collectionProcessor
+     * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
      * @param \Magento\Sales\Api\Data\OrderExtensionFactory|null $orderExtensionFactory
      */
     public function __construct(
         \Unific\Extension\Logger\Logger $logger,
         \Unific\Extension\Helper\Mapping $mapping,
         \Unific\Extension\Connection\Rest\Connection $restConnection,
-        \Magento\Sales\Api\Data\OrderSearchResultInterfaceFactory $searchResultFactory,
-        \Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface $collectionProcessor = null,
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
         \Magento\Sales\Api\Data\OrderExtensionFactory $orderExtensionFactory = null
     )
     {
-        $this->objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-
         $this->logger = $logger;
         $this->mappingHelper = $mapping;
         $this->restConnection = $restConnection;
 
-        $this->metadata = $this->objectManager->get(Magento\Sales\Model\ResourceModel\Metadata::class);
-        $this->searchResultFactory = $searchResultFactory;
-        $this->collectionProcessor = $collectionProcessor ?: $this->objectManager->get(\Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface::class);
-        $this->orderExtensionFactory = $orderExtensionFactory ?: $this->objectManager->get(\Magento\Sales\Api\Data\OrderExtensionFactory::class);
+        $this->orderRepository = $orderRepository;
+        $this->orderExtensionFactory = $orderExtensionFactory;
 
         parent::__construct($logger, $mapping, $restConnection);
     }
@@ -79,7 +45,7 @@ class OrderPlugin extends AbstractPlugin
     {
         foreach ($this->getRequestCollection('Magento\Sales\Api\OrderManagementInterface::place', 'before') as $request)
         {
-            $this->handleCondition($request->getId(), $request, $this->getFullOrder($order->getId()));
+            $this->handleCondition($request->getId(), $request,  $this->orderRepository->get($order->getId()));
         }
 
         return [$order];
@@ -94,25 +60,19 @@ class OrderPlugin extends AbstractPlugin
     {
         foreach ($this->getRequestCollection('Magento\Sales\Api\OrderManagementInterface::place') as $request)
         {
-            $this->handleCondition($request->getId(), $request, $this->getFullOrder($order->getId()));
+            $this->handleCondition($request->getId(), $request,  $this->orderRepository->get($order->getId()));
         }
 
         return $order;
     }
 
+    /**
+     * @param $id
+     */
     protected function getFullOrder($id)
     {
-        $order = $this->metadata->getNewInstance()->load($id);
-        $this->setShippingAssignments($order);
+        $order = $this->orderRepository->get($id);
 
-        return $order;
-    }
-    /**
-     * @param OrderInterface $order
-     * @return void
-     */
-    private function setShippingAssignments(OrderInterface $order)
-    {
         /** @var OrderExtensionInterface $extensionAttributes */
         $extensionAttributes = $order->getExtensionAttributes();
 
@@ -121,11 +81,16 @@ class OrderPlugin extends AbstractPlugin
         } elseif ($extensionAttributes->getShippingAssignments() !== null) {
             return;
         }
+
         /** @var ShippingAssignmentInterface $shippingAssignment */
-        $shippingAssignments = $this->getShippingAssignmentBuilderDependency();
+        $shippingAssignments = \Magento\Framework\App\ObjectManager::getInstance()->get(
+            \Magento\Sales\Model\Order\ShippingAssignmentBuilder::class
+        );
+
         $shippingAssignments->setOrderId($order->getEntityId());
         $extensionAttributes->setShippingAssignments($shippingAssignments->create());
         $order->setExtensionAttributes($extensionAttributes);
-    }
 
+        return $order;
+    }
 }
