918fe2f34ddd94b9027c884615267b1b7782c8a1
Ron van der Molen <ron@wizkunde.nl>
2018-09-13 13:15:26 +0200

    inject datamodel

--- Extension/Plugin/CartPlugin.php
+++ Extension/Plugin/CartPlugin.php
@@ -7,6 +7,30 @@ class CartPlugin extends AbstractPlugin
     protected $entity = 'cart';
     protected $subject = 'cart/create';
 
+    protected $dataObjectFactory;
+
+    /**
+     * OrderPlugin constructor.
+     * @param \Unific\Extension\Logger\Logger $logger
+     * @param \Unific\Extension\Helper\Mapping $mapping
+     * @param \Unific\Extension\Connection\Rest\Connection $restConnection
+     * @param \Magento\Framework\DataObjectFactory $dataObjectFactory
+     */
+    public function __construct(
+        \Unific\Extension\Logger\Logger $logger,
+        \Unific\Extension\Helper\Mapping $mapping,
+        \Unific\Extension\Connection\Rest\Connection $restConnection,
+        \Magento\Framework\DataObjectFactory $dataObjectFactory
+    )
+    {
+        $this->logger = $logger;
+        $this->mappingHelper = $mapping;
+        $this->restConnection = $restConnection;
+        $this->dataObjectFactory = $dataObjectFactory;
+
+        parent::__construct($logger, $mapping, $restConnection);
+    }
+
     /**
      * @param $subject
      * @param $email
@@ -17,7 +41,7 @@ class CartPlugin extends AbstractPlugin
     {
         foreach ($this->getRequestCollection('Magento\Customer\Api\AccountManagementInterface::isEmailAvailable', 'before') as $request)
         {
-            $emailObject = new \Magento\Framework\DataObject();
+            $emailObject = $this->dataObjectFactory->create();
             $emailObject->setEmail($email);
             $emailObject->setWebsiteId($websiteId);
 
@@ -37,7 +61,7 @@ class CartPlugin extends AbstractPlugin
     {
         foreach ($this->getRequestCollection('Magento\Customer\Api\AccountManagementInterface::isEmailAvailable') as $request)
         {
-            $emailObject = new \Magento\Framework\DataObject();
+            $emailObject = $this->dataObjectFactory->create();
             $emailObject->setEmail($email);
             $emailObject->setWebsiteId($websiteId);
 
