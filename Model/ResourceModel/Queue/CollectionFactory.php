7ad5b023ceb3e69074758e5cafa3d70980f0cab5
Ron van der Molen <ron@wizkunde.nl>
2018-10-04 13:55:36 +0200

    Reinstate removed factory

---
+++ Model/ResourceModel/Queue/CollectionFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Unific\Extension\Model\ResourceModel\Queue;
+
+class CollectionFactory
+{
+    /**
+     * Object Manager instance
+     *
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $_objectManager = null;
+
+    /**
+     * Instance name to create
+     *
+     * @var string
+     */
+    protected $_instanceName = null;
+
+    /**
+     * Factory constructor
+     *
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @param string $instanceName
+     */
+    public function __construct(\Magento\Framework\ObjectManagerInterface $objectManager, $instanceName = '\\Unific\\Extension\\Model\\ResourceModel\\Queue\\Collection')
+    {
+        $this->_objectManager = $objectManager;
+        $this->_instanceName = $instanceName;
+    }
+
+    /**
+     * Create class instance with specified parameters
+     *
+     * @param array $data
+     * @return \Unific\Extension\Model\ResourceModel\Request\Collection
+     */
+    public function create(array $data = array())
+    {
+        return $this->_objectManager->create($this->_instanceName, $data);
+    }
+}
