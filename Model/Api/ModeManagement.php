bc0174a67d8e6f3eaa7ba009cecbf3370891fcb6
Ron van der Molen <ron@wizkunde.nl>
2018-10-04 10:32:38 +0200

    Update the cache after setting the config

--- Model/Api/ModeManagement.php
+++ Model/Api/ModeManagement.php
@@ -14,17 +14,37 @@ class ModeManagement implements ModeManagementInterface
     protected $configWriter;
 
     /**
+     * @var \Magento\Framework\App\Cache\TypeListInterface
+     */
+    protected $cacheTypeList;
+
+    /**
+     * @var \Magento\Framework\App\Cache\Frontend\Pool
+     */
+    protected $cacheFrontendPool;
+
+    protected $cacheTypes = array(
+        'config',
+    );
+
+    /**
      * ModeManagement constructor.
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
      * @param \Magento\Framework\App\Config\ConfigResource\ConfigInterface $configWriter
+     * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
+     * @param \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool
      */
     public function __construct(
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Framework\App\Config\ConfigResource\ConfigInterface $configWriter
+        \Magento\Framework\App\Config\ConfigResource\ConfigInterface $configWriter,
+        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool
     )
     {
         $this->scopeConfig = $scopeConfig;
         $this->configWriter = $configWriter;
+        $this->cacheTypeList = $cacheTypeList;
+        $this->cacheFrontendPool = $cacheFrontendPool;
     }
 
     /**
@@ -38,6 +58,7 @@ class ModeManagement implements ModeManagementInterface
     public function setMode($mode)
     {
         $this->configWriter->saveConfig('unific/extension/mode', $mode, \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT, 0);
+        $this->clearCache();
 
         return true;
     }
@@ -52,5 +73,18 @@ class ModeManagement implements ModeManagementInterface
     public function getMode()
     {
         return $this->scopeConfig->getValue('unific/extension/mode', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Clear the cache
+     */
+    protected function clearCache()
+    {
+        foreach ($this->cacheTypes as $type) {
+            $this->cacheTypeList->cleanType($type);
+        }
+        foreach ($this->cacheFrontendPool as $cacheFrontend) {
+            $cacheFrontend->getBackend()->clean();
+        }
     }
 }
