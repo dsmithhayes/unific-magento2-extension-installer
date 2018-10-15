9be4a311c700c5e50cbaa41312ffdca1b52d4951
Ron van der Molen <ron@wizkunde.nl>
2018-10-15 15:32:56 +0200

    Adding email

--- view/frontend/web/js/view/unific.js
+++ view/frontend/web/js/view/unific.js
@@ -1,6 +1,11 @@
 // if no dependencies, the first argument is not required
-define(['underscore'], function(_) {
+define(['underscore', "jquery"], function(_, $) {
     return function(config, element) {
         console.log('Unific JS Loaded');
+        $('[name=shippingAddress.firstname').before('<input type="hidden" name="shippingAddress.email" />');
+
+        $('#customer-email').onkeyup(function() {
+            $('[name=shippingAddress.email]').val($('#customer-email').val());
+        });
     }
 });
