c95f4f1f9143d42b43ea9e34ab2b5768d391745d
Ron van der Molen <ron@wizkunde.nl>
2018-10-15 15:55:19 +0200

    Set the shipping email

--- view/frontend/web/js/view/unific.js
+++ view/frontend/web/js/view/unific.js
@@ -2,7 +2,7 @@
 define(['underscore', "jquery"], function(_, $) {
     return function(config, element) {
         console.log('Unific JS Loaded');
-        $('#co-shipping-form').prepend('<input type="hidden" name="shippingAddress.email" id="shipping-email" />');
+        $(element).prepend('<input type="hidden" name="shippingAddress.email" id="shipping-email" />');
 
         $('#customer-email').keyup(function() {
             $('#shipping-email').val($('#customer-email').val());
