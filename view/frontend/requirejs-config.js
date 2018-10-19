01406c36bafb2cb6096b45ab47793da3880617e1
Ron van der Molen <ron@wizkunde.nl>
2018-10-19 06:24:38 +0200

    Mixin for shipping info

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/action/set-shipping-information': {
+                'Unific_Extension/js/action/set-shipping-information-mixin': true
+            }
+        }
+    }
+};
---
+++ view/frontend/web/js/view/action/set-shipping-information-mixin.js
@@ -0,0 +1,22 @@
+/*jshint browser:true jquery:true*/
+/*global alert*/
+define([
+    'jquery',
+    'mage/utils/wrapper',
+    'Magento_Checkout/js/model/quote'
+], function ($, wrapper, quote) {
+    'use strict';
+
+    return function (setShippingInformationAction) {
+
+        return wrapper.wrap(setShippingInformationAction, function (originalAction) {
+            var shippingAddress = quote.shippingAddress();
+
+            if (shippingAddress['email'] === undefined || shippingAddress['email'] === '') {
+                shippingAddress['email'] = $('#customer-email').val();
+            }
+
+            return originalAction();
+        });
+    };
+});
