63464c44897d5506b2453250f78846a1260cacbe
Ron van der Molen <ron@wizkunde.nl>
2018-10-19 07:11:08 +0200

    Get the right shipping information

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,11 +1,8 @@
 var config = {
     config: {
         mixins: {
-            'Magento_Checkout/js/model/shipping-rate-processor/new-address': {
-                'Unific_Extension/js/model/shipping-rate-processor/new-address-mixin': true
-            },
-            'Magento_Checkout/js/model/shipping-rate-processor/customer-address': {
-                'Unific_Extension/js/model/shipping-rate-processor/customer-address-mixin': true
+            'Magento_Checkout/js/action/set-shipping-information': {
+                'Unific_Extension/js/action/set-shipping-information-mixin': true
             }
         }
     }
