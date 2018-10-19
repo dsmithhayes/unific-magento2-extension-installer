68cd027fc9b0ee1f37e04dae55b1255b74ed51b4
Ron van der Molen <ron@wizkunde.nl>
2018-10-19 07:27:18 +0200

    Remove double config layer

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,12 +1,10 @@
 var config = {
-    config: {
-        map: {
-            '*': {
-                "Magento_Checkout/js/model/shipping-rate-processor/new-address" :
-                    "Unific_Extension/js/shipping-rate-processor/new-address-override",
-                "Magento_Checkout/js/model/shipping-rate-processor/customer-address" :
-                    "Unific_Extension/js/shipping-rate-processor/customer-address-override"
-            }
+    map: {
+        '*': {
+            "Magento_Checkout/js/model/shipping-rate-processor/new-address" :
+                "Unific_Extension/js/shipping-rate-processor/new-address-override",
+            "Magento_Checkout/js/model/shipping-rate-processor/customer-address" :
+                "Unific_Extension/js/shipping-rate-processor/customer-address-override"
         }
     }
 };
