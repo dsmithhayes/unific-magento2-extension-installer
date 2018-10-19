1c45bf92d49fec57cf20ce4cc7ec278a37870b80
Ron van der Molen <ron@wizkunde.nl>
2018-10-19 07:28:55 +0200

    Path change

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,9 +2,9 @@ var config = {
     map: {
         '*': {
             "Magento_Checkout/js/model/shipping-rate-processor/new-address" :
-                "Unific_Extension/js/shipping-rate-processor/new-address-override",
+                "Unific_Extension/js/model/shipping-rate-processor/new-address-override",
             "Magento_Checkout/js/model/shipping-rate-processor/customer-address" :
-                "Unific_Extension/js/shipping-rate-processor/customer-address-override"
+                "Unific_Extension/js/model/shipping-rate-processor/customer-address-override"
         }
     }
 };
