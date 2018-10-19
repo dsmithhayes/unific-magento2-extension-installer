7561863b480903953783f133a439bbbc7cbeb6f7
Ron van der Molen <ron@wizkunde.nl>
2018-10-19 06:00:59 +0200

    Prepend the form element

--- view/frontend/web/js/view/unific.js
+++ view/frontend/web/js/view/unific.js
@@ -1,11 +1,18 @@
-// if no dependencies, the first argument is not required
-define(['underscore', "jquery"], function(_, $) {
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'underscore',
+    'mage/template'
+], function ($, _, mageTemplate) {
+    'use strict';
     return function(config, element) {
         console.log('Unific JS Loaded');
-        $(element).prepend('<input type="hidden" name="shippingAddress.email" id="shipping-email" />');
 
-        $('#customer-email').keyup(function() {
-            $('#shipping-email').val($('#customer-email').val());
-        });
+        var $formElement = ('<input type="hidden" name="shippingAddress.email" id="shipping-email" />');
+        $('#co-shipping-form').prepend($formElement);
     }
 });
